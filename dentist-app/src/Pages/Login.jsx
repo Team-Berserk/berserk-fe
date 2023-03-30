cc486590fd652439e724bb07fa709b64d6965575
22HOP2D-Enkhtuvshin <Enkhtuvshin@LEAP-7s-iMac.local>
2023-03-30 19:36:04 +0800

    LoginPage

--- dentist-app/src/Pages/Login.jsx
+++ dentist-app/src/Pages/Login.jsx
@@ -1,3 +1,30 @@
+import style from "./designs/Login.module.css"
+
 export const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className={style.container}>
+      <div className={style.loginContainer}>
+        <div className={style.img}></div>
+        <div className={style.loginInput}>
+          <h1 className={style.title}>Hello</h1>
+          <p className={style.p}>Login</p>
+          <div className={style.inputs}>
+            <div className={style.input}>
+              <span>Name</span>
+              <input className={style.miniInput} placeholder="Enter your full name" />
+            </div>
+            <div className={style.input}>
+              <span>Email</span>
+              <input className={style.miniInput} placeholder="Enter your email" />
+            </div> 
+            <div className={style.input}>
+              <span>Password</span>
+              <input className={style.miniInput} placeholder="Enter your password" />
+            </div>
+          </div>
+          <button>Login</button>
+        </div>
+      </div>
+    </div>
+  );
 };
