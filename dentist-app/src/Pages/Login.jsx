4ec78ea08db6943a5dffbbb92fb47be37220234d
Monho12 <oorgilmojo@gmail.com>
2023-03-31 19:01:06 +0800

    Login'nSignupFrontendDone

--- dentist-app/src/Pages/Login.jsx
+++ dentist-app/src/Pages/Login.jsx
@@ -1,30 +1,45 @@
-import style from "./designs/Login.module.css"
+import { Link } from "react-router-dom";
+import loginImg from "../Assets/Login.svg";
 
 export const Login = () => {
   return (
-    <div className={style.container}>
-      <div className={style.loginContainer}>
-        <div className={style.img}></div>
-        <div className={style.loginInput}>
-          <h1 className={style.title}>Hello</h1>
-          <p className={style.p}>Login</p>
-          <div className={style.inputs}>
-            <div className={style.input}>
-              <span>Name</span>
-              <input className={style.miniInput} placeholder="Enter your full name" />
-            </div>
-            <div className={style.input}>
-              <span>Email</span>
-              <input className={style.miniInput} placeholder="Enter your email" />
-            </div> 
-            <div className={style.input}>
-              <span>Password</span>
-              <input className={style.miniInput} placeholder="Enter your password" />
-            </div>
+    <>
+      <div className="flex flex-col p-28 justify-center items-center h-screen bg-sky-200">
+        <div className="fixed top-10 p-10">
+          <h1 className="text-3xl md:text-5xl">Soddent Dental Clinic</h1>
+        </div>
+        <div className="rounded-2xl flex p-10 justify-center items-center shadow-2xl gap-20 bg-sky-50 w-96 md:w-fit">
+          <div className="hidden md:block w-96">
+            <img src={loginImg} alt="loginImg" className="rounded-2xl" />
           </div>
-          <button>Login</button>
+          <div className="rounded-2xl">
+            <h1 className="text-center text-lg">Log In</h1>
+            <div className="flex flex-col h-fit w-80">
+              <h5>Email</h5>
+              <input
+                placeholder="email"
+                className="border-2 rounded-2xl p-2.5 outline-none focus:ring"
+              />
+              <h5>Usename</h5>
+              <input
+                placeholder="username"
+                className="border-2 rounded-2xl p-2.5 outline-none focus:ring"
+              />
+              <h5>Password</h5>
+              <input
+                placeholder="password"
+                className="border-2  rounded-2xl  p-2.5 outline-none focus:ring"
+              />
+              <button className="border-2 border-sky-400 rounded-2xl hover:bg-sky-400 active:bg-sky-600 focus:ring my-6 p-2.5">
+                Log In
+              </button>
+            </div>
+            <Link to="/signup" className="no-underline">
+              <div className="text-center">Dont have an account?</div>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
