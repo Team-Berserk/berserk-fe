a55a18c9c50418179857a8a367a775acf77c0ec7
Monho12 <oorgilmojo@gmail.com>
2023-04-01 21:01:06 +0800

    doneGenuine

--- dentist-app/src/Pages/Login.jsx
+++ dentist-app/src/Pages/Login.jsx
@@ -4,17 +4,17 @@ import loginImg from "../Assets/Login.svg";
 export const Login = () => {
   return (
     <>
-      <div className="flex flex-col p-28 justify-center items-center h-screen bg-sky-200">
-        <div className="w-80 md:w-fit fixed top-28">
-          <h1 className="text-3xl md:text-5xl text-center font-serif">
-            Soddent Dental Clinic
-          </h1>
-        </div>
-        <div className="rounded-2xl flex p-10 justify-center items-center shadow-2xl gap-20 bg-sky-50 w-80 md:w-fit">
+      <div className="flex flex-col p-28 justify-center items-center h-screen bg-teal-100">
+        <div className="rounded-2xl flex p-10 justify-center items-center gap-20 shadow-2xl bg-sky-50 w-80 md:w-fit">
           <div className="hidden md:block w-96">
             <img src={loginImg} alt="loginImg" className="rounded-2xl" />
           </div>
           <div className="rounded-2xl ">
+            <div className="w-72 md:w-80">
+              <h1 className="text-3xl md:text-5xl text-center font-serif">
+                Soddent Dental Clinic
+              </h1>
+            </div>
             <h1 className="text-center text-lg">Log In</h1>
             <div className="flex flex-col h-fit w-72 md:w-80">
               <h5 className="text-base md:text-lg">Usename</h5>
@@ -27,7 +27,7 @@ export const Login = () => {
                 placeholder="password"
                 className="border-2 rounded-2xl p-2.5 outline-none focus:ring"
               />
-              <button className="border-2 border-sky-400 rounded-2xl hover:bg-sky-400 active:bg-sky-600 focus:ring my-6 p-2.5">
+              <button className="bg-sky-300 rounded-2xl hover:bg-sky-400 active:bg-sky-500 my-6 p-2.5">
                 Log In
               </button>
             </div>
@@ -37,7 +37,7 @@ export const Login = () => {
           </div>
         </div>
         <div className="fixed bottom-5 ">
-          <div className="text-xs">Developed by NewJeans</div>
+          <div className="text-xs">Developed by Team Berserkers</div>
         </div>
       </div>
     </>
--- dentist-app/src/Pages/Signup.jsx
+++ dentist-app/src/Pages/Signup.jsx
@@ -4,17 +4,17 @@ import loginImg from "../Assets/SignUp.svg";
 export const Signup = () => {
   return (
     <>
-      <div className="flex flex-col p-28 justify-center items-center h-screen bg-sky-200">
-        <div className="w-80 md:w-fit fixed top-28">
-          <h1 className="text-3xl md:text-5xl text-center font-serif">
-            Soddent Dental Clinic
-          </h1>
-        </div>
-        <div className="rounded-2xl flex p-10 justify-center items-center shadow-2xl gap-20 bg-sky-50 w-80 md:w-fit">
+      <div className="flex flex-col p-28 justify-center items-center h-screen bg-teal-100">
+        <div className="rounded-2xl flex p-10 justify-center items-center gap-20 shadow-2xl bg-sky-50 w-80 md:w-fit">
           <div className="hidden md:block w-96">
             <img src={loginImg} alt="signupImg" className="rounded-2xl" />
           </div>
           <div className="rounded-2xl">
+            <div className="w-72 md:w-80">
+              <h1 className="text-3xl md:text-5xl text-center font-serif">
+                Soddent Dental Clinic
+              </h1>
+            </div>
             <h1 className="text-center text-lg">Sign Up</h1>
             <div className="flex flex-col h-fit w-72 md:w-80">
               <h5 className="text-base md:text-lg">Email</h5>
@@ -30,9 +30,9 @@ export const Signup = () => {
               <h5 className="text-base md:text-lg">Password</h5>
               <input
                 placeholder="password"
-                className="border-2  rounded-2xl p-2.5 outline-none focus:ring"
+                className="border-2 rounded-2xl p-2.5 outline-none focus:ring"
               />
-              <button className="border-2 border-sky-400 rounded-2xl active:bg-sky-600 focus:ring my-6 p-2.5">
+              <button className="bg-sky-300 rounded-2xl hover:bg-sky-400 active:bg-sky-500 my-6 p-2.5">
                 Sign Up
               </button>
             </div>
@@ -42,7 +42,7 @@ export const Signup = () => {
           </div>
         </div>
         <div className="fixed bottom-5 ">
-          <div className="text-xs">Developed by NewJeans</div>
+          <div className="text-xs">Developed by Team Berserkers</div>
         </div>
       </div>
     </>
