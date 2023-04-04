97bb3e7df36419bfba4ac96862659f890854866c
Monho12 <92088341+Monho12@users.noreply.github.com>
2023-04-04 17:51:45 +0800

    mongolz

--- dentist-app/src/Pages/Signup.jsx
+++ dentist-app/src/Pages/Signup.jsx
@@ -3,48 +3,46 @@ import loginImg from "../Assets/SignUp.svg";
 
 export const Signup = () => {
   return (
-    <>
-      <div className="flex flex-col p-28 justify-center items-center h-screen bg-teal-100">
-        <div className="rounded-2xl flex p-10 justify-center items-center gap-20 shadow-2xl bg-sky-50 w-80 md:w-fit">
-          <div className="hidden md:block w-96">
-            <img src={loginImg} alt="signupImg" className="rounded-2xl" />
-          </div>
-          <div className="rounded-2xl">
-            <div className="w-72 md:w-80">
-              <h1 className="text-3xl md:text-5xl text-center font-serif">
-                Soddent Dental Clinic
-              </h1>
-            </div>
-            <h1 className="text-center text-lg">Sign Up</h1>
-            <div className="flex flex-col h-fit w-72 md:w-80">
-              <h5 className="text-base md:text-lg">Email</h5>
-              <input
-                placeholder="email"
-                className="border-2 rounded-2xl p-2.5 outline-none focus:ring"
-              />
-              <h5 className="text-base md:text-lg">Usename</h5>
-              <input
-                placeholder="username"
-                className="border-2 rounded-2xl p-2.5 outline-none focus:ring"
-              />
-              <h5 className="text-base md:text-lg">Password</h5>
-              <input
-                placeholder="password"
-                className="border-2 rounded-2xl p-2.5 outline-none focus:ring"
-              />
-              <button className="bg-sky-300 rounded-2xl hover:bg-sky-400 active:bg-sky-500 my-6 p-2.5">
-                Sign Up
-              </button>
-            </div>
-            <Link to="/login" className="no-underline">
-              <div className="text-center">Already have an account?</div>
-            </Link>
-          </div>
+    <div className="flex flex-col p-28 justify-center items-center h-screen bg-teal-100">
+      <div className="rounded-2xl flex p-10 justify-center items-center gap-20 shadow-2xl bg-sky-50 w-80 md:w-fit">
+        <div className="hidden md:block w-96">
+          <img src={loginImg} alt="signupImg" className="rounded-2xl" />
         </div>
-        <div className="fixed bottom-5 ">
-          <div className="text-xs">Developed by Team Berserkers</div>
+        <div className="rounded-2xl">
+          <div className="w-72 md:w-80">
+            <h1 className="text-3xl md:text-5xl text-center font-serif">
+              Soddent Dental Clinic
+            </h1>
+          </div>
+          <h1 className="text-center text-lg">Бүртгүүлэх</h1>
+          <div className="flex flex-col h-fit w-72 md:w-80">
+            <h5 className="text-base md:text-lg">Имэйл</h5>
+            <input
+              placeholder="Имэйл"
+              className="border-2 rounded-2xl p-2.5 outline-none focus:ring"
+            />
+            <h5 className="text-base md:text-lg">Хэрэглэгчийн нэр</h5>
+            <input
+              placeholder="Хэрэглэгчийн нэр"
+              className="border-2 rounded-2xl p-2.5 outline-none focus:ring"
+            />
+            <h5 className="text-base md:text-lg">Нууц үг</h5>
+            <input
+              placeholder="Нууц үг"
+              className="border-2 rounded-2xl p-2.5 outline-none focus:ring"
+            />
+            <button className="bg-sky-300 rounded-2xl hover:bg-sky-400 active:bg-sky-500 my-6 p-2.5">
+              Бүртгүүлэх
+            </button>
+          </div>
+          <Link to="/login" className="no-underline">
+            <div className="text-center">Бүртгэлтэй юу?</div>
+          </Link>
         </div>
       </div>
-    </>
+      <div className="fixed bottom-5 ">
+        <div className="text-xs">Developed by Team Berserkers</div>
+      </div>
+    </div>
   );
 };
