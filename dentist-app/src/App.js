6c61f23bd1f89340d5b818113eaf5e277004afcb
Monho12 <92088341+Monho12@users.noreply.github.com>
2023-04-08 11:49:18 +0800

    ConnectFE

--- dentist-app/src/App.js
+++ dentist-app/src/App.js
@@ -1,6 +1,5 @@
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { AuthProvider } from './Providers'
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import {
   ClinicInfo,
   DoctorProfile,
@@ -9,32 +8,28 @@ import {
   Profile,
   Signup,
   Request,
-} from './Pages'
+} from "./Pages";
 
 function App() {
   return (
     <div className="h-screen">
-      <BrowserRouter>
-        <AuthProvider>
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/profile/">
-              <Route path=":id" element={<Profile />} />
-            </Route>
-            <Route path="/doctor/">
-              <Route path=":id" element={<DoctorProfile />} />
-            </Route>
-            <Route path="/clinicInfo" element={<ClinicInfo />} />
-            <Route path="/request">
-              <Route path=":id" element={<Request />} />
-            </Route>
-          </Routes>
-        </AuthProvider>
-      </BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile/">
+          <Route path=":id" element={<Profile />} />
+        </Route>
+        <Route path="/doctor/">
+          <Route path=":id" element={<DoctorProfile />} />
+        </Route>
+        <Route path="/clinicInfo" element={<ClinicInfo />} />
+        <Route path="/request">
+          <Route path=":id" element={<Request />} />
+        </Route>
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
