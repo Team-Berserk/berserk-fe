a2247a3cd99a5236743e20007fe7ad864b5ee053
Monho12 <92088341+Monho12@users.noreply.github.com>
2023-04-11 16:58:20 +0800

    Done

--- dentist-app/src/Components/Footer.jsx
+++ dentist-app/src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import FooterData from "./Json/FooterJson.json";
 
 export const Footer = () => {
   return (
-    <div className="border-2 border-rose-600 w-screen absolute bottom-0">
+    <div className="w-screen absolute bottom-0 bg-teal-50">
       <div className="flex justify-around items-center h-96">
         {/* Sections */}
 
@@ -18,7 +18,7 @@ export const Footer = () => {
 
         <div className="flex justify-end mx-16 gap-5 ">
           <div className=" w-56">
-            <div className="text-lg">Үйлчилгээ</div>
+            <div className="text-2xl my-3">Үйлчилгээ</div>
             {FooterData &&
               FooterData.Үйлчилгээ.map((item, index) => {
                 return (
@@ -28,8 +28,8 @@ export const Footer = () => {
                 );
               })}
           </div>
-          <div className="x w-56">
-            <div className="text-lg">Цагийн хуваарь</div>
+          <div className="w-56">
+            <div className="text-2xl my-3">Цагийн хуваарь</div>
             {FooterData &&
               FooterData.Цагийнхуваарь.map((item, index) => {
                 return (
@@ -39,18 +39,18 @@ export const Footer = () => {
                 );
               })}
           </div>
-          <div className="flex flex-col  w-56 gap-3">
+          <div className="flex flex-col w-56 gap-3 my-3">
             <div>
-              <div>ХАЯГ:</div>
-              <div>13-р хорооллын Наран дэлгүүртэй 3-н өндөрт</div>
+              <div className="text-xl">ХАЯГ:</div>
+              <div>· 13-р хорооллын Наран дэлгүүртэй 3-н өндөрт</div>
             </div>
             <div>
-              <div>УТАС:</div>
-              <div>7011 8778</div>
+              <di className="text-xl">УТАС:</di>
+              <div>· 7011 8778</div>
             </div>
             <div>
-              <div>Email: </div>
-              <div>soddent@gmail.com</div>
+              <div className="text-xl">Email: </div>
+              <div>· soddent@gmail.com</div>
             </div>
           </div>
         </div>
