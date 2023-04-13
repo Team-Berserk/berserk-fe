6f5130f934aa25f82105681dc7e41e24625a2ff3
Monho12 <92088341+Monho12@users.noreply.github.com>
2023-04-13 15:42:00 +0800

    editted

--- dentist-app/src/Pages/AdminDashboard.jsx
+++ dentist-app/src/Pages/AdminDashboard.jsx
@@ -1,5 +1,4 @@
 import { useContext } from "react";
-import { Link } from "react-router-dom";
 import { Card } from "../Components/RequestCard";
 import { DataContext } from "../Providers/DataContext";
 
@@ -8,17 +7,12 @@ export const Dashboard = () => {
   return (
     <div className="flex border-2 border-rose-500 h-screen">
       {console.log(allRequests)}
-      <div className="flex flex-col items-center w-96 h-20 border-2 border-teal-300 ">
+      <div className="flex flex-col items-center w-96 h-20 border-2 border-teal-300 justify-center">
         <h3 className="p-2.5 font-serif">LOGO (img)</h3>
-        <div className="border-2 border-green-400 flex w-full h-full p-4">
-          <Link to="/dashboard">
-            <div>Requests</div>
-          </Link>
-        </div>
       </div>
       <div className="w-screen">
         <div className="flex items-center justify-between border-2 shadow-sm w-full p-2 h-20">
-          <h3 className="m-1 font-serif">Admin Dashboard = Requests</h3>
+          <h3 className="m-1 font-serif">Requests</h3>
           <div className="flex items-center border-2 border-black rounded-4 h-8 px-3">
             <div className="text-xl font-serif">User</div>
           </div>
@@ -27,7 +21,6 @@ export const Dashboard = () => {
         <div className="overflow-y-scroll h-5/6 p-14 flex flex-wrap">
           {allRequests &&
             allRequests.map((req, index) => {
-              console.log(index);
               return <Card {...req} key={index} />;
             })}
         </div>
