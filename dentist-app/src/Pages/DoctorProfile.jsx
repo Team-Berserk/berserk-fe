6ba7a24529da6a2d6be744f1986a40194d42fbd4
Monho12 <92088341+Monho12@users.noreply.github.com>
2023-04-25 19:29:10 +0800

    doctor profile 50%

--- dentist-app/src/Pages/DoctorProfile.jsx
+++ dentist-app/src/Pages/DoctorProfile.jsx
@@ -1,3 +1,76 @@
+import Pro from "../Assets/Duure.svg";
+
 export const DoctorProfile = () => {
-  return <div>DOctor Octavius</div>;
+  return (
+    <div className="flex flex-col">
+      <div className="h-screen flex flex-col md:flex-row justify-center items-center md:justify-around">
+        <div className="flex flex-col gap-3">
+          <div className="flex flex-col gap-2">
+            <div className="text-5xl font-serif">Duurenbayar . O</div>
+            <div className="text-gray-400 text-sm">
+              delhiin shildeg shudnii emch boloh hun baigaa shuu
+            </div>
+          </div>
+          <div className="flex gap-6 font-serif text-lg">
+            <div>Bolzoh</div>
+            <div>Tsag awah</div>
+          </div>
+        </div>
+        <div>
+          <img src={Pro} className="w-80 md:w-fit" />
+        </div>
+      </div>
+      <div className="h-screen flex flex-col gap-52 justify-center">
+        <div className="justify-evenly items-center flex flex-col md:flex-row gap-3 md:gap-0">
+          <div className="font-mono text-4xl">ABOUT</div>
+          <div className="font-mono text-sm md:text-md w-96 md:w-1/2">
+            Hey. Manii neriig Duuree gedeg uur yu ch yarihav dee. Sain uu
+            naizuudaa sain uu naizuudaa, Bugdeeree hamt togli sahilgagui
+            poorooroo. Luugiin undug. free pic ashiglaad suga yum be Munkhuu17
+            batsaanaa??!!
+          </div>
+        </div>
+        <div className="justify-evenly items-center flex flex-col md:flex-row gap-3 md:gap-0">
+          <div className="font-mono text-4xl">SKILLS</div>
+          <div className="font-mono text-lg flex flex-col w-96 md:w-1/2 items-center">
+            <div className="w-2/3">
+              <div className="flex">
+                Surguuli:
+                <div className="flex gap-2 flex-col py-4">
+                  <div>1. Harvard</div>
+                  <div>2. Mvis</div>
+                </div>
+              </div>
+              <div className="flex w-full justify-center">
+                Chadwar:
+                <div className="flex gap-2 flex-col py-4">
+                  <div>1. Coding</div>
+                  <div>2. Scam art</div>
+                </div>
+              </div>
+              <div className="flex">
+                Awards:
+                <div className="flex gap-2 flex-col py-4">
+                  <div>1. Grammy</div>
+                  <div>2. 2x ballon’dor</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="justify-evenly items-center font-mono flex flex-col md:flex-row gap-3 md:gap-0">
+          <div className="text-4xl">WORKS</div>
+          <div className="text-sm flex flex-col md:flex-row gap-3 md:gap-20 items-center w-96 md:w-1/2">
+            <div>IMG</div>
+            <div className="flex flex-col gap-3 w-72 items-center">
+              <div className="text-3xl">Soddent Dentail Clinic</div>
+              <div className="text-gray-400">
+                Хэнтий аймгийн хамгийн гал эмнэлэг
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
