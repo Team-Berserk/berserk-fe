1a32677f924e2794c587a005aea501642648fac5
22PHOP2D-Duurenbayar <duurenbayarOwO@Nest-101s-iMac.local>
2023-05-09 18:30:47 +0800

    40404

--- dentist-app/src/Pages/PageNotFound.jsx
+++ dentist-app/src/Pages/PageNotFound.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export const PageNotFound = () => {
   return (
     <div className="h-screen flex items-center justify-center">
@@ -5,13 +7,15 @@ export const PageNotFound = () => {
         <h1 className="text-center text-8xl font-bold font-sans text-[#516eff]">
           404
         </h1>
-        <p className="text-center font-semibold text-lg sm:text-xl text-gray-600 p-4 w-full sm:w-[550px] sm:pt-5 sm:pb-4">
-          Tanii haij baigaa huudas byadnart naihgui zailTanii haij baigaa huudas
-          byadnart naihgui zail
+        <p className="text-center font-semibold text-lg sm  :text-xl text-gray-600 p-4 w-full sm:w-[550px] sm:pt-5 sm:pb-4">
+          Tanii oroh geed baigaa site chin bid nart yag unendee alga jich bi
+          textee urt bolgoh gej baahan yum bichiv.
         </p>
-        <button className="font-semibold text-white w-48 bg-[#516eff] p-4 rounded-xl">
-          Go To Homepage
-        </button>
+        <Link to="/">
+          <button className="font-semibold text-white w-48 bg-[#516eff] p-4 rounded-xl">
+            Go To Homepage
+          </button>
+        </Link>
       </div>
     </div>
   );
