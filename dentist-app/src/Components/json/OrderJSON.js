e95f209a38a2cfbc3b46c7cec3708ee5ac99f59f
22HOP2D-Enkhtuvshin <Enkhtuvshin@LEAP-7s-iMac.local>
2023-05-11 19:54:19 +0800

    fixed

---
+++ dentist-app/src/Components/json/OrderJSON.js
@@ -0,0 +1,27 @@
+export const OrderJSON = [
+    {
+        name: "yuch bsn yhve",
+        path: "idk",
+        clock: "1PM"
+    },
+    {
+        name: "yuch bsn yhve",
+        path: "idk",
+        clock: "1AM"
+    },
+    {
+        name: "yuch bsn yhve",
+        path: "idk",
+        clock: "1PM"
+    },
+    {
+        name: "yuch bsn yhve",
+        path: "idk",
+        clock: "1AM"
+    },
+    {
+        name: "yuch bsn yhve",
+        path: "idk",
+        clock: "1PM"
+    },
+]
