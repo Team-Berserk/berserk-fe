92bd4c170e84c144dd1ffd52ed29942ae0b56e67
duuree17 <duuren0315@gmail.com>
2023-05-11 14:57:09 +0800

    dashboard

--- dentist-app/src/Pages/AdminDashboard.jsx
+++ dentist-app/src/Pages/AdminDashboard.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 // import { DataContext } from "../Providers/DataContext";
 import { UserManager } from "../Components/UserManager";
 import { instance } from "../Clients";
@@ -7,16 +7,25 @@ import { utils } from "@amir04lm26/react-modern-calendar-date-picker";
 export const Dashboard = () => {
   // const { allRequests } = useContext(DataContext);
   const [users, setUsers] = useState(null);
+  const [awagdsn, setAwagdsn] = useState(0);
+  const Daragin = [];
+
   useEffect(() => {
     const day = utils().getToday();
     const date = `${day.year}-${day.month < 10 ? "0" : ""}${day.month}-${
       day.day < 10 ? "0" : ""
     }${day.day}`;
     instance.put("manage", { date }).then((response) => {
-      // console.log(response.data);
-      // console.log(utils().getToday());
-      // console.log(Object.entries(response.data.placeholder));
       setUsers(Object.entries(response.data.placeholder));
+      let num = 0;
+      console.log(Object.entries(response.data.placeholder));
+      Object.entries(response.data.placeholder).forEach((item) => {
+        if (item[1]._id) {
+          Daragin.push(item);
+          num++;
+        }
+      });
+      setAwagdsn(num);
     });
   }, []);
 
@@ -25,29 +34,31 @@ export const Dashboard = () => {
       <div className="w-full h-fit max-w-3xl">
         <h1 className=" font-bold text-xl">Soddent clinic admin dashboard.</h1>
         <div className="flex gap-8 pt-5">
-          <div className=" w-1/3 bg-blue-500 h-20 rounded-md"></div>
-          <div className=" w-1/3 bg-blue-500 h-20 rounded-md"></div>
-          <div className=" w-1/3 bg-blue-500 h-20 rounded-md"></div>
+          <div className=" w-1/3 p-3  bg-gradient-to-r from-cyan-500 to-blue-500 h-20 rounded-md">
+            <h3 className="text-2xl  px-2 font-bold text-white">{`${awagdsn}`}</h3>
+            <p className="text-white">Авагдсан цаг</p>
+          </div>
+          <div className=" w-1/3 p-3  bg-gradient-to-r from-cyan-500 to-blue-500 h-20 rounded-md">
+            <h3 className="text-2xl  px-2 font-bold text-white">{`${
+              8 - awagdsn
+            }`}</h3>
+            <p className="text-white">Сул цаг</p>
+          </div>
+          <div className=" w-1/3 p-3  bg-gradient-to-r from-cyan-500 to-blue-500 h-20 rounded-md">
+            <h3 className="text-2xl  px-2 font-bold text-white">69</h3>
+            <p className="text-white">Ямар нэг юм </p>
+          </div>
         </div>
         <h1 className=" font-bold text-xl mt-16 text-center m-5">
-          Unuudriin uilchluulegchid.
+          Өнөөдрийн үйлчлүүлэгчид
         </h1>
-        <div className="flex gap-2 mt-4">
-          <input
-            className="w-60 h-10 rounded-full border-solid border-gray-400 border-2 p-4 outline-none"
-            placeholder="Nereer haih"
-          />
-          <button className=" h-10 rounded-full border-solid border-gray-400 border-2 p-4 pt-1.5 flex align-top">
-            Angilah
-          </button>
-        </div>
         <div className="h-fit  bg-slate-200 mt-8 rounded-2xl pb-10">
           <div className="h-10 w-full flex align-middle  p-4 pt-2 pr-16">
-            <p className="w-32">User</p>
-            <p className="w-40">Attendance</p>
-            <p className="w-48">Phone Number</p>
-            <p className="w-32">Date</p>
-            <p className="w-42">Time</p>
+            <p className="w-32">Нэр</p>
+            <p className="w-40">Ирц</p>
+            <p className="w-48">Утасны Дугаар </p>
+            <p className="w-32">Өдөр</p>
+            <p className="w-42">Цаг</p>
           </div>
           {
             users &&
@@ -57,7 +68,10 @@ export const Dashboard = () => {
           }
         </div>
         <h1 className=" font-bold text-xl mt-16 text-center">
-          Daraagiin uilchluulegchid.
+          Дараагийн үйлчлүүлэгчид
+          {Daragin.map((user, index) => {
+            return <UserManager key={user._id} user={user} index={index} />;
+          })}
         </h1>
       </div>
     </div>
