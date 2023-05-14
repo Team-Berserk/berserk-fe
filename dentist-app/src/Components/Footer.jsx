5354fce32bf1f8270ea1a7fb2c139c15fd7ba0bc
Monho12 <oorgilmojo@gmail.com>
2023-05-14 18:55:38 +0800

    footer updated 2.0

--- dentist-app/src/Components/Footer.jsx
+++ dentist-app/src/Components/Footer.jsx
@@ -1,4 +1,8 @@
-import { FooterJSON } from "./json/FooterJSON";
+import clock from "../Assets/footerClock.svg";
+import calendar from "../Assets/footerCalendar.svg";
+import map from "../Assets/footerMap.svg";
+import headset from "../Assets/footerHeadset.svg";
+import facebook from "../Assets/footerFacebook.svg";
 import WebLogo from "../Assets/WebLogo.svg";
 
 export const Footer = () => {
@@ -15,26 +19,54 @@ export const Footer = () => {
               үргэлж хамгийн сайнаараа ажиллах болно .
             </div>
           </div>
-          <div className="my-3 md:m-0">LOGOS insta fabceovok link</div>
+          <a
+            href="https://www.facebook.com/profile.php?id=100057076555186"
+            target="_blank"
+          >
+            <div className="my-3 md:m-0">
+              <img alt="facebookImg" src={facebook} className="w-12" />
+            </div>
+          </a>
         </div>
         <div className="flex justify-center md:flex-row gap-10 py-5 md:p-0 md:items-center">
-          <div className="flex flex-col gap-2">
-            <h3>Products</h3>
-            {FooterJSON.map((item, index) => {
-              return <div key={index}>{item.name}</div>;
-            })}
+          <div className="flex flex-col gap-5 w-56">
+            <div className="flex gap-2 items-start">
+              <img src={map} />
+              <div>
+                <div className="text-lg">Xаяг</div>
+                <div>
+                  Khentii Aimag , PEARL DENTAL NYC233 Broadway18th FloorNew
+                  York, NY 10279
+                </div>
+              </div>
+            </div>
+            <div className="flex gap-2 items-start">
+              <img src={headset} />
+              <div>
+                <div className="text-lg">Holbogdoh</div>
+                <div>Phone : 9877 1177</div>
+              </div>
+            </div>
           </div>
-          <div className="flex flex-col gap-2">
-            <h3>Company</h3>
-            {FooterJSON.map((item, index) => {
-              return <div key={index}>{item.name}</div>;
-            })}
-          </div>
-          <div className="flex flex-col gap-2">
-            <h3>Donations</h3>
-            {FooterJSON.map((item, index) => {
-              return <div key={index}>{item.name}</div>;
-            })}
+          <div className="flex flex-col gap-12 w-56">
+            <div className="flex gap-4 items-start">
+              <img src={clock} />
+              <div>
+                <div className="text-lg">Schedule</div>
+                <div>Mon-Sat 10pm - 16:00</div>
+                <div>Mon-Sat 10pm - 16:00</div>
+                <div>Mon-Sat 10pm - 16:00</div>
+              </div>
+            </div>
+            <div>
+              <div className="flex gap-2 items-start">
+                <img src={calendar} />
+                <div>
+                  <div className="text-lg">Appointment</div>
+                  <div>Book appointment now!</div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
